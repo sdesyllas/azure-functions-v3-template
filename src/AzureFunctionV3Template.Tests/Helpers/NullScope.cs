b6020ffaa3a7f36201ee204c4c05a0656b649367
Spyros <sdesyllas@hotmail.com>
2020-07-14 10:46:04 +0100

    #1 Unit tests

---
+++ src/AzureFunctionV3Template.Tests/Helpers/NullScope.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AzureFunctionV3Template.Tests
+{
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+
+        private NullScope() { }
+
+        public void Dispose() { }
+    }
+}
---
+++ src/AzureFunctionV3Template.Tests/Helpers/TestFactory.cs
@@ -0,0 +1,33 @@
+﻿using AzureFunctionV3Template.Tests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Functions.Tests
+{
+    public class TestFactory
+    {
+        public static HttpRequest CreateHttpRequest()
+        {
+            var context = new DefaultHttpContext();
+            var request = context.Request;
+            return request;
+        }
+
+        public static ILogger CreateLogger(LoggerTypes type = LoggerTypes.Null)
+        {
+            ILogger logger;
+
+            if (type == LoggerTypes.List)
+            {
+                logger = new ListLogger();
+            }
+            else
+            {
+                logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+            }
+
+            return logger;
+        }
+    }
+}
